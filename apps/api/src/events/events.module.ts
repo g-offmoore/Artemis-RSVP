6dd1546b1dc31f046d59c3585b03d4ea1bd91e7f
Geoff Moore <airman.gmoore@yahoo.com>
2026-05-18 13:24:57 -0400

    Fix EventsModule provider dependencies

--- apps/api/src/events/events.module.ts
+++ apps/api/src/events/events.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
+import { AlertService } from "../common/alert.service.js";
+import { MetricsService } from "../metrics/metrics.service.js";
+import { PrismaService } from "../prisma/prisma.service.js";
 import { EventsController } from "./events.controller.js";
 import { EventsService } from "./events.service.js";
 
 @Module({
   controllers: [EventsController],
-  providers: [EventsService],
+  providers: [EventsService, PrismaService, MetricsService, AlertService],
   exports: [EventsService]
 })
 export class EventsModule {}
